c246690850555fb30ecf59926b585ccac44f8f12
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-05-03 15:15:54 +0100

    Updating server.js

---
+++ src/server.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const port = process.env.PORT || 5001
+
+const userRouter = require("./users/routes")
+const doneTodoRouter = require("./doneTodos/routes")
+const activeTodoRouter = require("./activeTodos/routes")
+
+const User = require("./users/model")
+const DoneTodo = require("./doneTodos/model")
+const ActiveTodo = require("./activeTodos/model")
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+console.log("checkpoint1")
+
+const syncTables = () => {
+    User.hasMany(DoneTodo)
+    User.hasMany(ActiveTodo)
+    DoneTodo.belongsTo(User)
+    ActiveTodo.belongsTo(User)
+    DoneTodo.sync({alter: true})
+    ActiveTodo.sync({alter: true})
+    User.sync()
+}
+
+console.log("checkpoint2")
+
+app.use(userRouter)
+app.use(doneTodoRouter)
+app.use(activeTodoRouter)
+
+console.log("checkpoint3")
+
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "api is working"})
+})
+
+app.listen(port, () => {
+    syncTables()
+    console.log(`server is running on port ${port}`)
+})
